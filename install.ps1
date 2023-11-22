c750632a4de6d1f5afe1cbb11185131f12470914
Aaron Stannard <aaron@petabridge.com>
2023-11-21 20:24:05 -0600

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,44 +1,43 @@
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 Set-TimeZone -Name "Central Standard Time" -Verbose
 
-cinst cmder -y
-cinst 7zip -y
-cinst jetbrainstoolbox -y
-cinst dotnet -y
-cinst dotnet-6.0-sdk -y
-cinst dotnet-8.0-sdk -y
-cinst dotnetfx -y
-cinst netfx-4.6.1-devpack -y
-cinst dropbox -y
-cinst sysinternals -y
-cinst gpg4win -y
-cinst brave -y
-cinst paint.net -y
-cinst gifcam -y
-cinst vscode -y
-cinst sublimetext3.app -y
-cinst slack -y
-cinst discord -y
-cinst docker-desktop -y
-cinst sqlitebrowser -y
-cinst markdownpad2 -y
-cinst linqpad -y
-cinst windirstat -y
-cinst pulumi -y
-cinst azure-cli -y
-cinst poshgit github-desktop -y
-cinst winmerge kdiff3 -y
+choco install cmder -y
+choco install 7zip -y
+choco install jetbrainstoolbox -y
+choco install dotnet -y
+choco install dotnet-6.0-sdk -y
+choco install dotnet-8.0-sdk -y
+choco install dotnetfx -y
+choco install netfx-4.6.1-devpack -y
+choco install dropbox -y
+choco install sysinternals -y
+choco install gpg4win -y
+choco install brave -y
+choco install paint.net -y
+choco install gifcam -y
+choco install vscode -y
+choco install sublimetext3.app -y
+choco install slack -y
+choco install discord -y
+choco install docker-desktop -y
+choco install sqlitebrowser -y
+choco install markdownpad2 -y
+choco install linqpad -y
+choco install windirstat -y
+choco install pulumi -y
+choco install azure-cli -y
+choco install poshgit github-desktop -y
+choco install winmerge kdiff3 -y
 
 # Data tools
-cinst azure-data-studio -y
-cinst sql-server-management-studio -y
+choco install azure-data-studio -y
+choco install sql-server-management-studio -y
 
 # Non-.NET development tools
-
-cinst msys2 -y
+choco install msys2 -y
 
 # Ruby Version Manager and Ruby Installation
-cinst rbenv -y
+choco install rbenv -y
 rbenv install 2.7.4 # Replace with the latest Ruby 2 version
 rbenv install 3.1.2 # Replace with the latest Ruby 3 version
 rbenv global 3.1.2
@@ -59,7 +58,7 @@ dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux 
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 # Visual Studio 2022 Installation
-cinst visualstudio2022professional -y
+choco install visualstudio2022professional -y
 
 Update-Help
 Install-WindowsUpdate
